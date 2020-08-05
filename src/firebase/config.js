6ece1045fa80680995b8c5e7f3dc555109c33b46
MarkMburu <markmburu685@gmail.com>
2020-08-05 15:08:31 +0300

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAeoKqw_huG5F3FQBHDirteHiZarvw8yiw",
+    authDomain: "mark-firegram.firebaseapp.com",
+    databaseURL: "https://mark-firegram.firebaseio.com",
+    projectId: "mark-firegram",
+    storageBucket: "mark-firegram.appspot.com",
+    messagingSenderId: "1046455925266",
+    appId: "1:1046455925266:web:aa95588699ce9fec9936cb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
